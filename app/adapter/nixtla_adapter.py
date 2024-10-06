af5cad4ffea548ea2c80a78e8d980d72cc0cb8b0
hungngo <qhung.bm0409@gmail.com>
2024-10-07 00:40:42 +0700

    feat: try without fine tunning

--- app/adapter/nixtla_adapter.py
+++ app/adapter/nixtla_adapter.py
@@ -19,10 +19,8 @@ def forecast(client, data: DataFrame) -> AnyDFType:
     return client.forecast(
         df=data,
         model="timegpt-1",
-        h=8,  # Forecast X days ahead
+        h=7,  # Forecast X days ahead
         level=[90],  # Generate a 90% confidence interval
-        finetune_steps=120,  # Specify the number of steps for fine-tuning
-        freq="B",
         time_col="Date",
         target_col="Adj Close",
     )
